d89633394c810f130bd141eed84724bfc521d636
Knufle <iamknufle@gmail.com>
2024-06-20 18:08:39 -0300

    tailwind and POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'accessibility-x-scroll';
+  cardsList = ["card1", "card2", "card3", "card4", "card5", "card6"];
+
+  onFocus(event: FocusEvent) {
+    console.log(event)
+  }
 }
